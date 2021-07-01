4352ad5d1741efd9e8561b5904190df170d57bb9
dylanbram354 <dylanbram354@gmail.com>
2021-07-01 10:27:29 -0400

    Installed CORS headers

--- flashcard_backend/flashcard_backend/settings.py
+++ flashcard_backend/flashcard_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'flashcards.apps.FlashcardsConfig'
+    'flashcards.apps.FlashcardsConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'flashcard_backend.urls'
@@ -123,3 +125,6 @@ try:
     from flashcard_backend.local_settings import *
 except ImportError:
     pass
+
+
+CORS_ORIGIN_ALLOW_ALL=True
